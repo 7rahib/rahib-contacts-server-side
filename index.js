d2c88e8daab7c3cb0cd895f6a3031fcbf5334901
7rahib <rahibrashid002@gmail.com>
2022-09-22 10:46:31 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -9,15 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tu4gevq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
-
+        await client.connect();
+        console.log('DB Connected');
     }
     finally {
 
     }
 }
 
+run().catch(console.dir);
 
 // Root API
 app.get('/', (req, res) => {
