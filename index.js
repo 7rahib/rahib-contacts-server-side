7e86939cf48bffd75831dbb9a907bccc7380ae27
7rahib <rahibrashid002@gmail.com>
2022-10-16 01:24:36 +0600

    Label name added to contact api created

--- index.js
+++ index.js
@@ -231,6 +231,23 @@ async function run() {
             res.send(result);
         });
 
+
+        // Adding Labels to contact
+        app.put("/labels/:id", async (req, res) => {
+            const id = req.params.id;
+            const UpdateUser = req.body.label;
+            const filter = { _id: ObjectId(id) };
+            const option = { upset: true };
+            const updateDoc = {
+                $set: {
+                    label: UpdateUser,
+                },
+            };
+            const result = await contactsCollection.updateOne(filter, updateDoc, option);
+            res.send(result);
+        });
+
+
         // Label Ends
     }
     finally {
